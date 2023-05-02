5fd7a0448e9507258fca34c4266c72eaec070855
MJephat <mukundijephat22@gmail.com>
2023-05-02 15:56:47 +0300

    navbar

--- techmart/src/App.js
+++ techmart/src/App.js
@@ -1,15 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
-
-
-function App() {
-  return (
-    <div className="App">
-      <h1>𝙏𝙀𝘾𝙃𝙈𝘼𝙍𝙏</h1>
-      
-  
-    </div>
-=======
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar"
 import menu from "./components/menu"
@@ -28,7 +17,6 @@ function App() {
         <Route path='/singup' component={signup} />
       </Routes>
     </Router>
->>>>>>> eb5c2be (navbar)
   );
 }
 
