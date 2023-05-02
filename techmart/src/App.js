50db660a47738a24445aa4d48a780e01576b32bb
MJephat <mukundijephat22@gmail.com>
2023-05-02 10:27:09 +0300

    started the app

---
+++ techmart/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>𝙏𝙀𝘾𝙃𝙈𝘼𝙍𝙏</h1>
+      
+   
+    </div>
+  );
+}
+
+export default App;
