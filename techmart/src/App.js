9d8f4ae9a3fa116b38b11ebf0d3277ab12388a6d
Kelly985 <86683835+Kelly985@users.noreply.github.com>
2023-05-02 15:51:00 +0300

    deployed json data

--- techmart/src/App.js
+++ techmart/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Login from './components/Login';
+
 
 function App() {
   return (
     <div className="App">
       <h1>𝙏𝙀𝘾𝙃𝙈𝘼𝙍𝙏</h1>
       
-   
+  
     </div>
   );
 }
