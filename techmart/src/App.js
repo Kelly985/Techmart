b48fb4363967c14a4a68ecbd497f342e5813abc5
iqra-ali <iqra.ali@student.moringaschool.com>
2023-05-07 17:32:29 +0300

    something

--- techmart/src/App.js
+++ techmart/src/App.js
@@ -1,12 +1,17 @@
+import React, { useEffect, useState } from "react";
 import './App.css';
-import Login from './components/Login';
+import Card from "./components/Card";
+import Carousel from "./components/Carousel";
+
+
+
 
 function App() {
   return (
     <div className="App">
       <h1>𝙏𝙀𝘾𝙃𝙈𝘼𝙍𝙏</h1>
-      
-   
+      <Carousel />
+      <Card />
     </div>
   );
 }
